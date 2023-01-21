cd0ba8568c5f658bfce2d12532ff99eceb6d0d04
Kevin Dahlhoff <kdahlhoff3@gmail.com>
2023-01-21 19:05:35 +0100

    Added While Loop and exit condition

--- SimpleLogin/Program.cs
+++ SimpleLogin/Program.cs
@@ -8,34 +8,38 @@ namespace SimpleLogin
 {
     public class SimpleLogin
     {
-        public static readonly string _encryptKey = "b14ca5898a4e4133bbce2ea2315a1916";
+        public static readonly string _encryptKey = "";
 
         public static void Main()
         {
-            Console.WriteLine("Input Login : 1 | Register : 2");
-            int start = short.Parse(Console.ReadLine() ?? "1");
-
-            string username = GetUsername();
-            string password = GetPassword();
-
-            switch (start)
+            bool main = true;
+            while (main)
             {
-                case 1:
-                    if (CheckUserCredential(username, password)) Console.WriteLine("Login successfull!");
-                    else Console.WriteLine("Login failed! Username or Password incorrect");
-                    break;
+                Console.WriteLine("Input Exit : 0 | Login : 1 | Register : 2");
+                int start = short.Parse(Console.ReadLine() ?? "1");
 
-                case 2:
-                    if (!CheckIfUserExists(username))
-                    {
-                        AddNewUser(username, password);
-                        Console.WriteLine("Successfully registered!");
-                    }
-                    else Console.WriteLine("An User with this Username already exists");
-                    break;
+                string username = GetUsername();
+                string password = GetPassword();
 
-                default:
-                    break;
+                switch (start)
+                {
+                    case 1:
+                        if (CheckUserCredential(username, password)) Console.WriteLine("Login successfull!");
+                        else Console.WriteLine("Login failed! Username or Password incorrect");
+                        break;
+
+                    case 2:
+                        if (!CheckIfUserExists(username))
+                        {
+                            AddNewUser(username, password);
+                            Console.WriteLine("Successfully registered!");
+                        }
+                        else Console.WriteLine("An User with this Username already exists");
+                        break;
+
+                    default:
+                        break;
+                }
             }
         }
 
